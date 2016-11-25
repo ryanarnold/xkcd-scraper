351a5ee823dfa2897dab94720eeeb3d20a5dcec8
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2016-11-25 16:29:35 +0800

    Add selectors for collecting comic details

--- xkcd_scraper/spiders/comics.py
+++ xkcd_scraper/spiders/comics.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from xkcd_scraper.items import ComicItem
-
+from hashlib import sha1
 
 class ComicsSpider(scrapy.Spider):
     name = "comics"
@@ -10,6 +10,12 @@ class ComicsSpider(scrapy.Spider):
 
     def parse(self, response):
         comic = ComicItem()
-        comic['file_urls'] = ['https:' + response.xpath('//div[@id="comic"]/img/@src').extract_first(),]
+        comic['title'] = response.xpath('//div[@id="ctitle"]/text()').extract_first()
+        comic['desc'] = response.xpath('//div[@id="comic"]/img/@title').extract_first()
+
+        comic_url = 'https:' + response.xpath('//div[@id="comic"]/img/@src').extract_first()
+
+        comic['filename'] = sha1(comic_url.encode('utf-8')).hexdigest() + '.jpg'
+        comic['file_urls'] = [comic_url,]
         
         yield comic
